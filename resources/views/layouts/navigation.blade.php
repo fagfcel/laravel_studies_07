08b457dbcc9b7779c5800bf2559596f4e2dccf55
Francisco Ferreira <estudosfagf@gmail.com>
2025-06-24 21:06:17 -0300

    003

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('nova_pagina')" :active="request()->routeIs('nova_pagina')">
+                        {{ __('Nova Página') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
